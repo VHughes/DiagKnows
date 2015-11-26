cd2d8f4e76320c548998349ad9e453c448e0554b
VHughes <virginia.hughes04@gmail.com>
2015-11-26 09:59:19 -0800

    Isla went ham here.

--- server.php
+++ server.php
@@ -2,6 +2,8 @@
 session_start();
 include("connection.php");
 
+   
+
 function Register(){
 	global $con;
 	$query = "INSERT INTO username (email, password) VALUES ('" .$_POST['email']."', '".$_POST['password']."')";
@@ -15,11 +17,25 @@ function Register(){
 function FBRegister(){
 	global $con;
 	$query = "INSERT INTO username (email, uName, fbID, img) VALUES ('" .$_POST['email']."', '".$_POST['name']."', '".$_POST['fb_id']."', '".$_POST['img']."')";
+    $result = mysqli_query($con, $query);
     //echo query;
-    $_SESSION['login'] = true;
-	$result = mysqli_query($con, $query);
+   /* $_SESSION['login'] = true;
+    
+	
+    
+    
+    
+    $table = "SELECT * FROM username WHERE uName='".$name."'";
+    var_dump($query);
+   
+    
+    //var_dump($results);
+    
+    */
+    
 	if($result){
 		echo json_encode("INSERTED");
+   
 	}
 }
 
@@ -28,11 +44,34 @@ function FBIDSelect(){
 	$query = "SELECT * FROM username WHERE fbID = '".$_POST['fb_id']."'";
 	$result = mysqli_query($con, $query);
 	if($result){
-		$row = mysqli_fetch_array($result);
+    $user = "root";
+    $pass = "root";
+    $db = new PDO('mysql:host=localhost;dbname=illness', $user, $pass);
+
+    $conn = $db; 
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        $row = mysqli_fetch_array($result);
 		echo json_encode($row);
+         $_SESSION['login'] = true;
+        $name = $_POST['fb_id'];
+        $table = "SELECT * FROM username WHERE uName='".$name."'";
+         $sqli = $conn->query($table);
+        $results = $sqli->fetchAll();
+        var_dump($results);
+        exit;
+        $_SESSION['username'] = $results[0]['uName'];
+        $_SESSION['lastname'] = $results[0]['lname'];
+        $_SESSION['email'] = $results[0]['email'];
+        echo $_SESSION['username'];
+    
 	}
 	
 }
+
+
+
+
 
 if($_POST['mode'] == 0){
 	Register();
@@ -40,10 +79,17 @@ if($_POST['mode'] == 0){
 
 if($_POST['mode'] == 1){
 	FBRegister();
+    
+   
+    
 }
 
 if($_POST['mode'] == 2){
 	FBIDSelect();
+    
+    //$_SESSION['username'] = $results[0]['uName'];
+    //var_dump($results);
+    //$results = sqli->fetchAll();
 }
 
 ?>
