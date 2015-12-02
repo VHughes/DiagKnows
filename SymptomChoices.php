a2bce6311a91cdaf8f316f2e6c50ba97dd266d1e
isla-mcc <mcisla@hotmail.com>
2015-12-02 05:14:00 -0800

    oops forgot to add this one #AlmostHadAHeartattack

--- SymptomChoices.php
+++ SymptomChoices.php
@@ -8,20 +8,16 @@ $db = "illness";
 $conn = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-$illness = array('Cold','Food Poisoning','Flu'); 
+$name = "SELECT illness.ill_name, illness.Ill FROM illness_symptom LEFT JOIN illness ON illness_symptom.ILL_ID=illness.ILL WHERE SYM_ID= :number";
 
-$name = "SELECT illness.ill_name FROM illness_symptom LEFT JOIN illness ON illness_symptom.ILL_ID=illness.ILL";
-$sqli = $conn->query($name);
+$number = $_POST['checked'][0];
+
+$sqli = $conn->prepare($name);
+$sqli->execute(array(
+    ":number"=>$number
+));
+
 $result = $sqli->fetchAll();
-//echo "<pre>";
-//var_dump($result);
-//echo "</pre>";
-    
-$_GET['cold'] = $result[0]['ill_name'];
-$_GET['foodpoison'] = $result[8]['ill_name'];
-$_GET['flu'] = $result[13]['ill_name'];
 
-echo "haha " .$_GET['cold']. " yup " .$_GET['flu']. " whoops";
-//$sql = $conn->query($name);
-//echo($sql);
+echo json_encode($result);
 ?>
